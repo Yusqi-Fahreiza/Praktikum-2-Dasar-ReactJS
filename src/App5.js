e0a28fe78e1c22f633b3abc0164f9f1fd7b75c9c
Yusqi Fahreiza <68527680+Yusqi-Fahreiza@users.noreply.github.com>
2020-09-08 12:52:30 +0700

    Add files via upload

---
+++ src/App5.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';  
+import './App.css';  
+import Media from './Components/Media';  
+// import element Media yang telah dibuat  
+  
+class App extends Component {  
+  render(){  
+    return (
+        <div className="App container col-sm-6">
+          <Media image="React.png" title="React JS">  
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus  
+            scelerisque ante sollicitudin. Cras purus odio, vestibulum  
+            in vulputate at, tempus viverra turpis  
+          </Media>  
+          <Media image="Angular.png" title="Angular JS">  
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus  
+            scelerisque ante sollicitudin. Cras purus odio, vestibulum  
+            in vulputate at, tempus viverra turpis  
+          </Media>  
+        </div>  
+      );
+  }  
+}  
+
+export default App;
+
+        
